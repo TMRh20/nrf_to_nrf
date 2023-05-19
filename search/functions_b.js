e5bb477e72c531b1b11581c5dcdc5db42142d670
TMRh20 <tmrh20@gmail.com>
2023-05-19 07:41:22 -0600

    Update docs

--- search/functions_b.js
+++ search/functions_b.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['testcarrier_0',['testCarrier',['../classnrf__to__nrf.html#aef67812f5e62734fb8215161174b3fe0',1,'nrf_to_nrf']]],
-  ['testrpd_1',['testRPD',['../classnrf__to__nrf.html#a58d3be7502b81f507caa4cef436a4c1a',1,'nrf_to_nrf']]],
+  ['testcarrier_0',['testCarrier',['../classnrf__to__nrf.html#a1b0d3c0393baaec2963e325dca708935',1,'nrf_to_nrf']]],
+  ['testrpd_1',['testRPD',['../classnrf__to__nrf.html#a4de30b154367928dae7b778b21748d53',1,'nrf_to_nrf']]],
   ['txstandby_2',['txStandBy',['../classnrf__to__nrf.html#a1b2f567dda77209301cbaea4c5a75862',1,'nrf_to_nrf::txStandBy()'],['../classnrf__to__nrf.html#a8c249d4566bb6b6219b22b69c46daa9d',1,'nrf_to_nrf::txStandBy(uint32_t timeout, bool startTx=0)']]]
 ];
