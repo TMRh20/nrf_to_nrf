b83e1f22672b1cc7fb774d661085ed8fefd2b770
TMRh20 <TMRh20@users.noreply.github.com>
2026-06-20 14:01:27 +0000

    deploy: cb1874f1b6c0c5a8402329fb41c98f22f16f4d25

--- html/search/functions_d.js
+++ html/search/functions_d.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['waitforevent_0',['waitForEvent',['../nrf__to__nrf_8cpp.html#aafd2b50d97614bab7e21436f907c1e55',1,'nrf_to_nrf.cpp']]],
+  ['waitforevent_0',['waitForEvent',['../nrf__to__nrf_8cpp.html#a6a37685aaa008c5b12b89698521a5d23',1,'nrf_to_nrf.cpp']]],
   ['write_1',['write',['../classnrf__to__nrf.html#a4b72553f78465e14ba15c13a392fa7e3',1,'nrf_to_nrf']]],
   ['writeackpayload_2',['writeAckPayload',['../classnrf__to__nrf.html#a7b9b9b7116aab9d2723f7a317c1ae766',1,'nrf_to_nrf']]],
   ['writefast_3',['writeFast',['../classnrf__to__nrf.html#ac23bd0686bcfafada06a668ba32bcbd7',1,'nrf_to_nrf']]]
